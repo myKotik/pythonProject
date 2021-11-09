157d6cddb584d5a774db0164af56be16d3d409cc
Kate <Lebedeva.kate6518@gmail.com>
2021-11-09 12:53:28 +0200

    two

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 import random
 
-attempts = 0
+print("Rock, Paper, Scissors")
 
-number = random.randint(1, 20)
-print("I am thinking of a number between 1 and 20.")
 
-while attempts < 5:
-    guess = input("Take a guess ")
-    guess = int(guess)
-    attempts +=1
+def try_again():
+    rps = ["Rock", "Paper", "Scissors"]
+    computer = random.choice(rps)
 
-    if guess < number:
-        print("Higher")
+    player = input("your choice: ").lower().capitalize()
 
-    if guess > number:
-        print("Lower")
+    if computer =="Rock":
+        if player =="Rock":
+            print(f"I chose {computer}, you chose {player}\nit's a tie!")
+        elif player == "Paper":
+                print(f"I chose {computer}, you chose {player}\nYou win!")
+        elif player =="Scissors":
+            print(f"I chose {computer}, you chose {player}\n You lost!")
+    elif computer =="Paper":
+        if player =="Rock":
+            print(f"I chose {computer}, you chose {player}\n You lost!")
+        elif player == "Paper":
+                print(f"I chose {computer}, you chose {player}\nit's a tie!")
+        elif player =="Scissors":
+            print(f"I chose {computer}, you chose {player}\nYou win!")
+    elif computer =="Scissors":
+        if player =="Rock":
+            print(f"I chose {computer}, you chose {player}\nYou win!")
+        elif player == "Paper":
+                print(f"I chose {computer}, you chose {player}\n You lost!")
+        elif player =="Scissors":
+            print(f"I chose {computer}, you chose {player}\nit's a tie!")
+    play_again = input("Do you want to play again? yes or no: ").lower().capitalize()
+    # If the player says yes, go back to the function
+    if play_again == "Yes":
+        try_again()
+    # If the player says no, say goodbye
+    elif play_again == "No":
+        print("Goodbye")
 
-    if number == guess:
-        print("You win")
-        break
-if guess == number:
-        attempts = str(attempts)
-        print(f"Good job! You guessed my number in {attempts} guesses!")
-if guess != number:
-        number = str(number)
-        print(f"Nope. The number I was thinking of was {number}")
+
+# End of function
+try_again()
